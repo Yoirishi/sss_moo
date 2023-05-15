e162e1e1a6ccdb243034044658b9ed143c591556
Yoirishi <undertaker_zdv@mail.ru>
2023-05-15 12:03:43 +1000

    optimaize memory allocation with sorting buffer

--- src/optimizers/age_moea2.rs
+++ src/optimizers/age_moea2.rs
@@ -38,7 +38,12 @@ struct SortingBuffer<S>
     last_front_indicies: Vec<usize>,
     ens_fronts: Vec<Vec<usize>>,
     ens_indicies: Vec<usize>,
-    flat_fronts: Vec<Candidate<S>>
+    flat_fronts: Vec<Candidate<S>>,
+    points: Vec<Vec<f64>>,
+    point_indicies_by_front: Vec<Vec<usize>>,
+    selected_fronts: Vec<bool>,
+    final_population: Vec<Candidate<S>>,
+    best_candidates: Vec<(Vec<f64>, S)>
 }
 
 pub struct AGEMOEA2Optimizer<'a, S: Solution> {
@@ -63,6 +68,7 @@ impl<'a, S> AGEMOEA2Optimizer<'a, S>
     /// Instantiate a new optimizer with a given meta params
     pub fn new(meta: impl Meta<'a, S> + 'a) -> Self {
         let points_on_first_front: Vec<Vec<f64>> = Vec::with_capacity(meta.population_size());
+        let selected_fronts: Vec<bool> = Vec::with_capacity(meta.population_size());
 
         AGEMOEA2Optimizer {
             meta: Box::new(meta),
@@ -78,6 +84,11 @@ impl<'a, S> AGEMOEA2Optimizer<'a, S>
                 ens_fronts: vec![],
                 ens_indicies: vec![],
                 flat_fronts: vec![],
+                points: vec![],
+                point_indicies_by_front: vec![],
+                selected_fronts,
+                final_population: vec![],
+                best_candidates: vec![]
             },
         }
     }
@@ -104,7 +115,9 @@ impl<'a, S> AGEMOEA2Optimizer<'a, S>
     }
 
     #[allow(clippy::needless_range_loop)]
-    fn sort(&mut self, pop: Vec<Candidate<S>>) -> Vec<Candidate<S>> {
+    fn sort(&mut self, initial_pop: Option<&Vec<Candidate<S>>>) -> () {
+        let pop = initial_pop.unwrap_or(&self.sorting_buffer.final_population);
+
         self.sorting_buffer.objs.clear();
         for cand in pop.iter()
         {
@@ -134,18 +147,31 @@ impl<'a, S> AGEMOEA2Optimizer<'a, S>
 
         debug_assert!(!self.sorting_buffer.flat_fronts.is_empty());
 
-        let (clear_fronts, points) = AGEMOEA2Optimizer::separate_fronts_and_points(&self, &self.sorting_buffer.flat_fronts);
-
-        let indicies = concatenate_matrix_rows(&clear_fronts);
+        self.sorting_buffer.point_indicies_by_front.clear();
+        self.sorting_buffer.points.clear();
+        self.separate_fronts_and_points();
+        let clear_fronts = &self.sorting_buffer.point_indicies_by_front;
+        let points = &self.sorting_buffer.points;
 
         let prepared_fronts = &mut self.sorting_buffer.prepared_fronts;
         prepared_fronts.clear();
-        for index in indicies
+        for front in &self.sorting_buffer.point_indicies_by_front
         {
-            prepared_fronts.push(self.sorting_buffer.flat_fronts[index].clone());
+            for point_index in front
+            {
+                prepared_fronts.push(self.sorting_buffer.flat_fronts[*point_index].clone());
+            }
         }
 
-        for (new_front_rank, indicies_of_candidate) in clear_fronts.iter().enumerate()
+        self.sorting_buffer.best_candidates.clear();
+        for index in clear_fronts[0].iter()
+        {
+            prepared_fronts[*index].front = 0;
+            self.sorting_buffer.best_candidates.push((self.sorting_buffer.points[*index].clone(), prepared_fronts[*index].sol.clone()))
+        }
+
+
+        for (new_front_rank, indicies_of_candidate) in clear_fronts.iter().enumerate().skip(1)
         {
             for index in indicies_of_candidate
             {
@@ -174,10 +200,21 @@ impl<'a, S> AGEMOEA2Optimizer<'a, S>
             self.sorting_buffer.points_on_first_front.push(points[*index].clone())
         }
 
+        for i in &mut self.sorting_buffer.selected_fronts {
+            *i = false;
+        }
 
-        let mut selected_fronts = prepared_fronts.iter()
-            .map(|candidate| candidate.front < max_front_no)
-            .collect();
+        for (point_index, candidate) in  prepared_fronts.iter().enumerate()
+        {
+            if candidate.front < max_front_no
+            {
+                while self.sorting_buffer.selected_fronts.len() <= point_index
+                {
+                    self.sorting_buffer.selected_fronts.push(false)
+                }
+                self.sorting_buffer.selected_fronts[point_index] = true
+            }
+        }
 
         self.sorting_buffer.crowding_distance.clear();
         self.sorting_buffer.crowding_distance.extend((0..prepared_fronts.len()).map(|_| 0.));
@@ -249,25 +286,29 @@ impl<'a, S> AGEMOEA2Optimizer<'a, S>
             );
         rank.reverse();
 
-        let count_of_selected = mask_positive_count(&selected_fronts);
+        let count_of_selected = mask_positive_count(&self.sorting_buffer.selected_fronts);
         let n_surv = self.meta.population_size();
 
         let count_of_remaining = n_surv - count_of_selected;
         for i in 0..count_of_remaining
         {
-            selected_fronts[self.sorting_buffer.last_front_indicies[rank[i]]] = true
+            while self.sorting_buffer.selected_fronts.len() <= self.sorting_buffer.last_front_indicies[rank[i]]
+            {
+                self.sorting_buffer.selected_fronts.push(false)
+            }
+            self.sorting_buffer.selected_fronts[self.sorting_buffer.last_front_indicies[rank[i]]] = true
         }
 
-        let mut result = Vec::with_capacity(n_surv);
-        for (child_index, is_survive) in selected_fronts.iter().enumerate()
+
+        self.sorting_buffer.final_population.clear();
+
+        for (child_index, is_survive) in self.sorting_buffer.selected_fronts.iter().enumerate()
         {
             if *is_survive
             {
-                result.push(prepared_fronts[child_index].clone());
+                self.sorting_buffer.final_population.push(prepared_fronts[child_index].clone());
             }
         }
-
-        result
     }
 
     #[allow(clippy::borrowed_box)]
@@ -295,25 +336,19 @@ impl<'a, S> AGEMOEA2Optimizer<'a, S>
         vals.iter().all(|(v1, v2)| v1 <= v2) && vals.iter().any(|(v1, v2)| v1 < v2)
     }
 
-    fn separate_fronts_and_points(&self, candidates: &Vec<Candidate<S>>) -> (Vec<Vec<usize>>, Vec<Vec<f64>>)
+    fn separate_fronts_and_points(&mut self) -> ()
     {
-        let mut fronts = vec![];
-        let mut points = vec![];
-        for (candidate_index,candidate) in candidates.iter().enumerate()
+        for (candidate_index,candidate) in self.sorting_buffer.flat_fronts.iter().enumerate()
         {
-            points.push(self.values(&candidate.sol));
+            self.sorting_buffer.points.push(self.values(&candidate.sol));
             let front_id = candidate.front;
-            while front_id >= fronts.len()
+            while front_id >= self.sorting_buffer.point_indicies_by_front.len()
             {
-                fronts.push(vec![]);
+                self.sorting_buffer.point_indicies_by_front.push(vec![]);
             }
-            fronts[front_id].push(candidate_index)
+            self.sorting_buffer.point_indicies_by_front[front_id].push(candidate_index)
         }
-
-        return (fronts, points)
     }
-
-
 }
 
 fn newton_raphson(points: &Vec<Vec<f64>>, extreme_points_indicies: &Vec<usize>) -> f64 {
@@ -856,7 +891,7 @@ impl<'a, S> Optimizer<S> for AGEMOEA2Optimizer<'a, S>
         }
         runtime_solutions_processor.new_candidates(preprocess_vec);
 
-        let mut parent_pop = self.sort(pop);
+        self.sort(Some(&pop));
 
         for iter in 0.. {
             if runtime_solutions_processor.needs_early_stop()
@@ -867,22 +902,26 @@ impl<'a, S> Optimizer<S> for AGEMOEA2Optimizer<'a, S>
             runtime_solutions_processor.iteration_num(iter);
 
             self.best_solutions.clear();
-            parent_pop
-                .iter()
-                .take_while(|c| c.front == 0)
-                .for_each(|mut c| {
-                    let vals: Vec<f64> = self.values(&c.sol);
+            for solution in self.sorting_buffer.best_candidates.iter() {
+                self.best_solutions.push(solution.clone())
+            }
 
-                    self.best_solutions.push((vals, c.sol.clone()));
-                });
+            // self.sorting_buffer.final_population
+            //     .iter()
+            //     .filter(|c| c.front == 0)
+            //     .for_each(|&c| {
+            //         let vals: Vec<f64> = self.values(&c.sol);
+            //
+            //         self.best_solutions.push((vals, c.sol.clone()));
+            //     });
 
             runtime_solutions_processor.iter_solutions(
-                parent_pop.iter_mut()
+                self.sorting_buffer.final_population.iter_mut()
                 .map(|child| &mut child.sol)
                 .collect()
             );
 
-            if parent_pop
+            if self.sorting_buffer.final_population
                 .iter()
                 .map(|c| {
                     self.meta
@@ -896,16 +935,16 @@ impl<'a, S> Optimizer<S> for AGEMOEA2Optimizer<'a, S>
                 break;
             }
 
-            if eval.can_terminate(iter, parent_pop.iter().map(|c| self.values(&c.sol)).collect())
+            if eval.can_terminate(iter, self.sorting_buffer.final_population.iter().map(|c| self.values(&c.sol)).collect())
             {
                 break;
             }
 
             while child_pop.len() < pop_size {
-                let p1 = parent_pop.choose_mut(&mut rnd).unwrap().clone();
-                let p2 = parent_pop.choose_mut(&mut rnd).unwrap().clone();
-                let p3 = parent_pop.choose_mut(&mut rnd).unwrap().clone();
-                let p4 = parent_pop.choose_mut(&mut rnd).unwrap().clone();
+                let p1 = self.sorting_buffer.final_population.choose_mut(&mut rnd).unwrap().clone();
+                let p2 = self.sorting_buffer.final_population.choose_mut(&mut rnd).unwrap().clone();
+                let p3 = self.sorting_buffer.final_population.choose_mut(&mut rnd).unwrap().clone();
+                let p4 = self.sorting_buffer.final_population.choose_mut(&mut rnd).unwrap().clone();
 
                 let mut c1 = self.tournament(p1, p2);
                 let mut c2 = self.tournament(p3, p4);
@@ -929,10 +968,10 @@ impl<'a, S> Optimizer<S> for AGEMOEA2Optimizer<'a, S>
                 child_pop.push(c2);
             }
 
-            runtime_solutions_processor.extend_iteration_population(&parent_pop.iter_mut()
+            runtime_solutions_processor.extend_iteration_population(&self.sorting_buffer.final_population.iter_mut()
                 .map(|child| &mut child.sol)
                 .collect(),
-                                                                    &mut extended_solutions_buffer);
+                &mut extended_solutions_buffer);
 
             while let Some(solution) = extended_solutions_buffer.pop()
             {
@@ -954,12 +993,12 @@ impl<'a, S> Optimizer<S> for AGEMOEA2Optimizer<'a, S>
 
             while let Some(candidate) = child_pop.pop()
             {
-                parent_pop.push(candidate);
+                self.sorting_buffer.final_population.push(candidate);
             }
 
-            let sorted = self.sort(parent_pop);
 
-            parent_pop = sorted;
+
+            self.sort(None);
         }
     }
 
